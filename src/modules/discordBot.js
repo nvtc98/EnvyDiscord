f2a99026950377128f64913fe5583d0b75adb878
nvtc.98 <nvtc.98@gmail.com>
2021-07-14 17:30:06 +0700

    deploy

--- src/modules/discordBot.js
+++ src/modules/discordBot.js
@@ -29,3 +29,29 @@ bot.on("ready", async () => {
 bot.on("message", (msg) => {
   parseMessage(bot, msg);
 });
+
+// bot.on("guildMemberAdd", (member) => {
+//   const channel = member.guild.channels.cache.find((ch) => {
+//     console.log(ch);
+//   });
+//   console.log("member", member, channel);
+//   if (!channel) return;
+//   channel.send(`Bầu trời thì xanh biếc,`);
+//   channel.send(`${member} thì vào rạp xiếc!`);
+//   channel.send(`Hajimemashite, ${member} ^^`);
+//   channel.send(
+//     `https://image.myanimelist.net/ui/5pjpFizOF0WqHWXSGonzMWlyScxw1Dp5y-h9v_vvWt3CpaPd9w8YoGHyeQVCXT7tlJwHhC1emkSd7IV4F1b0pgMZOXLCMl_HgCC6lYwmHeM`
+//   );
+// });
+
+// bot.on("guildMemberRemove", (member) => {
+//   const channel = member.guild.channels.cache.find(
+//     (ch) => ch.name === "member-log"
+//   );
+//   if (!channel) return;
+//   channel.send(`Chotto-, ${member} rời khỏi rạp xiếc ư`);
+//   channel.send(
+//     `https://64.media.tumblr.com/6ebf792c1e396c1546a133eef46337f3/tumblr_onhjo0dDu01qa94xto1_540.gifv`
+//   );
+//   channel.send(`Ara ara, nói xấu ${member} thôi Ｏ(≧▽≦)Ｏ`);
+// });
--- src/modules/voice.js
+++ src/modules/voice.js
@@ -8,14 +8,18 @@ module.exports = async (bot) => {
   const connection = await voiceChannel
     .join()
     .catch(() => console.log("Không thể truy cập voice channel."));
+  if (!connection) {
+    return;
+  }
   let dispatcher = connection.play(
     ytdl("https://www.youtube.com/watch?v=a-Nj6S74MoI", { filter: "audioonly" })
   );
-  dispatcher.on("finish", () => {
+  const restart = () => {
     dispatcher = connection.play(
       ytdl("https://www.youtube.com/watch?v=a-Nj6S74MoI", {
         filter: "audioonly",
       })
     );
-  });
+  };
+  dispatcher.on("finish", restart);
 };
