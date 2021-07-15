6a3950ec37177f6bb51bc0846d7fcf1f1920a391
nvtc.98 <nvtc.98@gmail.com>
2021-07-15 10:20:17 +0700

    deploy

--- src/modules/voice.js
+++ src/modules/voice.js
@@ -5,23 +5,23 @@ const ytdl = require("ytdl-core");
 
 module.exports = async (bot) => {
   const voiceChannel = bot.channels.cache.get(idle);
-  const connection = await voiceChannel
-    .join()
-    .catch(() => console.log("Không thể truy cập voice channel."));
-  if (!connection) {
-    return;
-  }
-  let dispatcher = connection.play(
-    ytdl("https://www.youtube.com/watch?v=a-Nj6S74MoI", { filter: "audioonly" })
-  );
-  const restart = () => {
-    console.log("restart");
-    dispatcher = connection.play(
-      ytdl("https://www.youtube.com/watch?v=a-Nj6S74MoI", {
-        filter: "audioonly",
-      })
-    );
-    dispatcher.on("finish", restart);
-  };
-  dispatcher.on("finish", restart);
+  // const connection = await voiceChannel
+  //   .join()
+  //   .catch(() => console.log("Không thể truy cập voice channel."));
+  // if (!connection) {
+  //   return;
+  // }
+  // let dispatcher = connection.play(
+  //   ytdl("https://www.youtube.com/watch?v=a-Nj6S74MoI", { filter: "audioonly" })
+  // );
+  // const restart = () => {
+  //   console.log("restart");
+  //   dispatcher = connection.play(
+  //     ytdl("https://www.youtube.com/watch?v=a-Nj6S74MoI", {
+  //       filter: "audioonly",
+  //     })
+  //   );
+  //   dispatcher.on("finish", restart);
+  // };
+  // dispatcher.on("finish", restart);
 };
